82cae925f9a24781d96757094204e06abaaac705
Tallulah Kay <tkay@bu.edu>
2019-04-17 19:20:33 -0400

    fixed provenance

--- carlosp_jpva_tkay_yllescas/sampling.py
+++ carlosp_jpva_tkay_yllescas/sampling.py
@@ -140,32 +140,36 @@ class sampling(dml.Algorithm):
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('carlosp_jpva_tkay_yllescas', 'carlosp_jpva_tkay_yllescas')
+        
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
-        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
-        this_script = doc.agent('alg:carlosp_jpva_tkay_yllescas#registered_sd', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        get_registered = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_registered, this_script)
-        doc.usage(get_registered, resource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'?type=Registered&$select=type,latitude,longitude,OPEN_DT'
-                  }
-                  )
+        this_script = doc.agent('alg:carlosp_jpva_tkay_yllescas#sampling', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        get_sampling = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
 
-        registered = doc.entity('dat:carlosp_jpva_tkay_yllescas#registered_sd', {prov.model.PROV_LABEL:'Registered Voters', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(registered, this_script)
-        doc.wasGeneratedBy(registered, get_registered, endTime)
-        doc.wasDerivedFrom(registered, resource, get_registered, get_registered, get_registered)
+        eligible_resource = doc.entity('dat:carlosp_jpva_tkay_yllescas#eligible_ward',  {prov.model.PROV_LABEL: 'Eligible Voters', prov.model.PROV_TYPE: 'ont:DataSet'})
+        registered_resource = doc.entity('dat:carlosp_jpva_tkay_yllescas#registered_by_ward',  {prov.model.PROV_LABEL: 'Registered Voters', prov.model.PROV_TYPE: 'ont:DataSet'})
+        
+        sampling = doc.entity('dat:carlosp_jpva_tkay_yllescas#sampling', {prov.model.PROV_LABEL:'Sampling', prov.model.PROV_TYPE:'ont:DataSet'})
+            
+        doc.wasAssociatedWith(get_sampling, this_script)
+        
+        doc.usage(get_sampling, eligible_resource, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Computation'})
+
+        doc.usage(get_sampling, registered_resource, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Computation'})
+        
+        doc.wasAttributedTo(sampling, this_script)
+        doc.wasDerivedFrom(sampling, eligible_resource, get_sampling, get_sampling, get_sampling)
+        doc.wasDerivedFrom(sampling, registered_resource, get_sampling, get_sampling, get_sampling)
 
         repo.logout()
                   
         return doc
-    
-sampling.execute()
+## sampling.execute()
 
 '''
 # This is example code you might use for debugging this module.
